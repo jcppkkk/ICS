005997a9de1fc63db538db4b72bf581082e07784
尚哲 <comet.jc@gmail.com>
2009-06-06 18:31:44 +0800

    add TASKexample

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ function print_r_xml($arr,$wrapper = 'data',$cycle = 1)
     $new_line = "\n";
 
     //start building content
-    if($cycle == 1) { $output = '<?xml version="1.0" encoding="UTF-8" ?>'.$new_line; }
+    /*if($cycle == 1) { $output = '<?xml version="1.0" encoding="UTF-8" ?>'.$new_line; }*/
     $output.= tabify($cycle - 1).'<'.$wrapper.'>'.$new_line;
     foreach($arr as $key => $val)
     {
@@ -72,9 +72,32 @@ print_r($shellcmd);
 
 ?>
 <form  method="post" action="">
-<input type="hidden" name="task[1][time]" value=1988/01/01 />
-<input type="hidden" name="task[2][time]" value=1988/01/01 />
-<input type="hidden" name="task[3][time]" value=1988/01/01 />
+<input type="hidden" name="task[1][id]" value=6526815602157 />
+<input type="hidden" name="task[1][op]" value=+ />
+<input type="hidden" name="task[1][at][mon]" value=10 />
+<input type="hidden" name="task[1][at][day]" value=10 />
+<input type="hidden" name="task[1][at][week]" value=-1 />
+<input type="hidden" name="task[1][at][hour]" value=17 />
+<input type="hidden" name="task[1][at][min]" value=45 />
+<input type="hidden" name="task[1][do]" value="tar -zcvf back.tgz WWW" />
+
+<input type="hidden" name="task[2][id]" value=6526815602157 />
+<input type="hidden" name="task[2][op]" value=- />
+<input type="hidden" name="task[2][at][mon]" value=-1 />
+<input type="hidden" name="task[2][at][day]" value=-1 />
+<input type="hidden" name="task[2][at][week]" value=-1 />
+<input type="hidden" name="task[2][at][hour]" value=-1 />
+<input type="hidden" name="task[2][at][min]" value=15 />
+<input type="hidden" name="task[2][do]" value="tar -zcvf back.tgz WWW" />
+
+<input type="hidden" name="task[3][id]" value=6526815602157 />
+<input type="hidden" name="task[3][op]" value=! />
+<input type="hidden" name="task[3][at][mon]" value=-1 />
+<input type="hidden" name="task[3][at][day]" value=-1 />
+<input type="hidden" name="task[3][at][week]" value=-1 />
+<input type="hidden" name="task[3][at][hour]" value=-1 />
+<input type="hidden" name="task[3][at][min]" value=15 />
+<input type="hidden" name="task[3][do]" value="tar -zcvf back.tgz WWW" />
   <table border="1">
     <tr>
       <td>目前指令</td>
