2c6bd203ed09ec82956bf7a151b5eb84ff3e9ccb
尚哲 <comet.jc@gmail.com>
2009-06-15 13:55:22 +0800

    .

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>test</title>
+<script type="text/javascript" src="./js/jquery-dynamic-form.js"></script>
 </head>
 <body>
 <?php
@@ -14,38 +15,9 @@ $b = getCache();
 #echo "<br>after----------------------------------------<br>";
 #print_r($b);
 ?>
-
-<form  method="post" action="">
-<input type="hidden" name="task[1][id]" value=6526815602157 />
-<input type="hidden" name="task[1][op]" value=+ />
-<input type="hidden" name="task[1][at][mon]" value=10 />
-<input type="hidden" name="task[1][at][day]" value=10 />
-<input type="hidden" name="task[1][at][week]" value=-1 />
-<input type="hidden" name="task[1][at][hour]" value=17 />
-<input type="hidden" name="task[1][at][min]" value=45 />
-<input type="hidden" name="task[1][do]" value="tar -zcvf back.tgz WWW" />
-
-<input type="hidden" name="task[2][id]" value=6526815602157 />
-<input type="hidden" name="task[2][op]" value=- />
-<input type="hidden" name="task[2][at][mon]" value=-1 />
-<input type="hidden" name="task[2][at][day]" value=-1 />
-<input type="hidden" name="task[2][at][week]" value=-1 />
-<input type="hidden" name="task[2][at][hour]" value=-1 />
-<input type="hidden" name="task[2][at][min]" value=15 />
-<input type="hidden" name="task[2][do]" value="tar -zcvf back.tgz WWW" />
-
-<input type="hidden" name="task[3][id]" value=6526815602157 />
-<input type="hidden" name="task[3][op]" value=! />
-<input type="hidden" name="task[3][at][mon]" value=-1 />
-<input type="hidden" name="task[3][at][day]" value=-1 />
-<input type="hidden" name="task[3][at][week]" value=-1 />
-<input type="hidden" name="task[3][at][hour]" value=-1 />
-<input type="hidden" name="task[3][at][min]" value=15 />
-<input type="hidden" name="task[3][do]" value="tar -zcvf back.tgz WWW" />
+<form name="input_form" id="input_form"  method="post" action="">
   <table border="1">
     <tr>
-      <td>目前指令</td>
-      <td><?php echo $shellcmd; ?></td>
 	<td>分</td>
 	<td>
 	<SELECT NAME="minute_shellcmd">
