7c8ecd206a2879ef0052fa760e8faaada8cc6fcb
尚哲 <comet.jc@gmail.com>
2009-06-20 16:28:41 +0800

    Install.sh

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 (cd api;make clean;make)
-mkdir  coretask
-chmod o+w php/UIcache
-chmod o+w coretask
+(mkdir  coretask ; chmod o+w coretask;) 2> /dev/null
+(mkdir php/UIcache; chmod o+w php/UIcache; rm php/UIcache/*;) 2> /dev/null
+
